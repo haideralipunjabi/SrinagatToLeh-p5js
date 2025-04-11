b841ab410e81e581e728f629914c333f8e11bc96
Haider Ali Punjabi <me@haideralipunjabi.com>
2025-04-12 01:55:56 +0530

    added images

--- sketch.js
+++ sketch.js
@@ -5,12 +5,14 @@ let images = [];
 let canvas;
 let padding = 20;
 let canvasSize;
-let totalSteps = 0;
+let teamData = {};
 let font;
 let logo;
 let strokeWeightSize = 20;
-const goal = 2500000;
-async function getTotalSteps() {
+const goal = 500000;
+
+let teamConsts = {};
+async function getTeamData() {
     const sheetID = '1NyK1nFiYTe2GoOh0ldIEsebYTHMUWnAZsRp0jAOAuek';
     const sheetURL = `https://docs.google.com/spreadsheets/d/${sheetID}/gviz/tq?tqx=out:json&sheet=Sheet1`;
 
@@ -20,21 +22,26 @@ async function getTotalSteps() {
             const json = JSON.parse(data.substring(47).slice(0, -2));
             const headers = json.table.cols.map(col => col.label?.trim().toLowerCase());
             const rows = json.table.rows;
+            const teamIndex = headers.indexOf('team');
             const stepsIndex = headers.indexOf('steps tracked');
 
-            let totalSteps = 0;
+            let teamTotals = {}
 
             rows.forEach(row => {
                 const stepsCell = row.c[stepsIndex];
-
-                if (stepsCell) {
+                const teamCell = row.c[teamIndex];
+                if (stepsCell && teamCell) {
                     const steps = parseInt(stepsCell.v, 10);
+                    const team = teamCell.v.trim();
                     if (!isNaN(steps)) {
-                        totalSteps += steps;
+                        teamTotals[team] = (teamTotals[team] || 0) + steps;
                     }
                 }
             });
-            return totalSteps;
+            const entries = Object.entries(teamTotals);
+
+            const sortedEntries = entries.sort((a, b) => b[1] - a[1]);
+            return Object.fromEntries(sortedEntries);
         });
 }
 
@@ -46,7 +53,29 @@ function preload() {
         loadImage("/assets/drass.png"),
         loadImage("/assets/kargil.png"),
         loadImage("/assets/ladakh.png")
-    ]
+    ];
+    teamConsts = {
+        "Cycos": {
+            "color": [255, 182, 193],
+            "image": loadImage("/assets/cycos.png")
+        },
+        "Akal se paidal": {
+            "color": [174, 198, 207],
+            "image": loadImage("/assets/asp.png")
+        },
+        "Walkie Talkies": {
+            "color": [119, 221, 119],
+            "image": loadImage("/assets/wt.png")
+        },
+        "Walking Dead": {
+            "color": [255, 255, 153],
+            "image": loadImage("/assets/wd.png")
+        },
+        "Legs Miserables": {
+            "color": [203, 192, 255],
+            "image": loadImage("/assets/lm.png")
+        }
+    }
     font = loadFont('/assets/font.ttf');
     logo = loadImage("/assets/logo.png");
 
@@ -60,8 +89,8 @@ function setup() {
     canvas = createCanvas(canvasSize, canvasSize,);
     centerCanvas();
     strokeCap(ROUND);
-    textFont(font);
-    textSize(canvasSize / 25);
+    // textFont(font);
+    textSize(canvasSize / 50);
     textAlign(CENTER, CENTER);
     pixelDensity(10);
 
@@ -82,7 +111,51 @@ function centerCanvas() {
     let y = (windowHeight - height) / 2;
     canvas.position(x, y);
 }
+
+function drawTeamPlot(teamName, samplePoints) {
+    let progress = teamData[teamName] / goal;
+    let teamColor = color(...teamConsts[teamName]["color"]);
+    console.log(teamColor)
+    if (progress > 2) {
+        progress = 2;
+    }
+    let progress2 = 0;
+    if (progress > 1.0) {
+        progress2 = progress - 1;
+        progress = 1;
+    }
+    let progressCount = floor(progress * samplePoints.length);
+    let progressCount2 = floor(progress2 * samplePoints.length);
+
+
+    console.log(samplePoints.length);
+
+    stroke(teamColor);
+    strokeWeight(strokeWeightSize);
+    noFill();
+    beginShape();
+    for (let i = 0; i < progressCount; i++) {
+        let pt = samplePoints[i];
+        vertex(pt.x, pt.y);
+    }
+    endShape();
+
+    beginShape();
+    for (let i = 0; i < progressCount2; i++) {
+        let pt = samplePoints[samplePoints.length - 1 - i];
+        vertex(pt.x, pt.y);
+    }
+    lastPoint = samplePoints[progressCount];
+    if (progress2 != 0) {
+        lastPoint = samplePoints[samplePoints.length - 1 - progressCount2];
+    }
+    image(teamConsts[teamName]["image"], lastPoint.x - (canvasSize / 30), lastPoint.y - (canvasSize / 30), canvasSize / 15, canvasSize / 15,);
+
+    endShape();
+}
+
 function draw() {
+    if (Object.keys(teamData).length == 0) return;
     background(229, 217, 200);
     strokeWeightSize = canvasSize / 30;
 
@@ -125,39 +198,9 @@ function draw() {
     }
     samplePoints.push(pts[pts.length - 1]);
 
-    let progress = totalSteps / goal;
-    if (progress > 2) {
-        progress = 2;
+    for (team of Object.keys(teamData)) {
+        drawTeamPlot(team, samplePoints)
     }
-    let progress2 = 0;
-    if (progress > 1.0) {
-        progress2 = progress - 1;
-        progress = 1;
-    }
-    let progressCount = floor(progress * samplePoints.length);
-    let progressCount2 = floor(progress2 * samplePoints.length);
-
-
-
-    stroke(31, 58, 67);
-    strokeWeight(strokeWeightSize);
-    noFill();
-    beginShape();
-    for (let i = 0; i < progressCount; i++) {
-        let pt = samplePoints[i];
-        vertex(pt.x, pt.y);
-    }
-    endShape();
-
-    stroke(255, 0, 0);
-    strokeWeight(strokeWeightSize);
-    noFill();
-    beginShape();
-    for (let i = 0; i < progressCount2; i++) {
-        let pt = samplePoints[samplePoints.length - 1 - i];
-        vertex(pt.x, pt.y);
-    }
-    endShape();
 
     fill(248, 155, 53);
     noStroke();
@@ -174,13 +217,14 @@ function draw() {
         image(images[i], pts[i].x + offsets[i].x, pts[i].y + offsets[i].y, canvasSize / 7, canvasSize / 7);
     }
     image(logo, canvasSize * 0.85 - ((canvasSize / 5) / 2), canvasSize * 0.62, canvasSize / 5, canvasSize / 5,);
-
-    text("Steps:", canvasSize * 0.85, canvasSize * 0.85);
-    text(totalSteps, canvasSize * 0.85, canvasSize * 0.9);
+    // text("Steps:", canvasSize * 0.85, canvasSize * 0.85);
+    // text(totalSteps, canvasSize * 0.85, canvasSize * 0.9);
+    noLoop();
 }
 function saveCanvasImage() {
     saveCanvas(canvas, 'srinagar-to-leh', 'png');
 }
 window.addEventListener("DOMContentLoaded", async () => {
-    totalSteps = (await getTotalSteps());
+    teamData = (await getTeamData());
+    console.log(teamData)
 });
